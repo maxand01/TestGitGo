b3546405a4b79ef09b93c5ada37273e5c48ecab5
maxime roussel <maxime.roussel@2isa.org>
2018-11-08 11:27:40 +0100

    Ajout classe Personne

--- TestGitGo/Program.cs
+++ TestGitGo/Program.cs
@@ -10,6 +10,13 @@ namespace TestGitGo
     {
         static void Main(string[] args)
         {
+            Personne p1 = new Personne();
+            p1.Nom = "Ketchum";
+            p1.Prenom = "Sacha";
+            Console.WriteLine(p1.Nom + " " + p1.Prenom);
+
+            Console.ReadKey();
+
         }
     }
 }
