187b1c5d87fcbf0eead0c276d851dc26daf7eeb6
Fabien Allard <fabien.allard.83@orange.fr>
2018-11-08 14:21:44 +0100

    Ajout classe salarie

---
+++ TestGitGo/Salarie.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGitGo
+{
+  public class Salarie : Personne
+  {
+    public Salarie(string nom, string prenom, int salaire) : base(nom, prenom)
+    {
+      Sal = salaire;
+    }
+
+    public Salarie(Personne p1, int salaire)
+    {
+      Sal = salaire;
+    }
+
+    public int Sal { get; set; }
+  }
+}
